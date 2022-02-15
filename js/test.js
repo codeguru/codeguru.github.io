2334abb3dafcda94a0d8e7f078988b87b2b3d54b
mykola <mykola@web.dev>
2022-02-15 11:53:57 +0200

    first commit

---
+++ js/test.js
@@ -0,0 +1,13 @@
+// Generate random string in JavaScript
+
+function makeId(length) {
+  var result = '';
+  var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+console.log(makeId(15)); // for example: w523nu2v1nyndzj
